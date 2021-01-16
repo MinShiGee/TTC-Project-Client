f3c50c1f651ba04c202897eff33558a5521fe145
MinShiGee <alstjr1642@ajou.ac.kr>
2021-01-16 21:05:36 +0900

    bug fix

---
+++ Assets/Scripts/Lobby/RoomInfo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomInfoDto
+{
+    public int id;
+    public string roomName;
+    public string ownerName;
+
+    public int curPlayers;
+    public int maxPlayers;
+
+    public RoomInfoDto(int _id, string _roomName, string _ownerName, int _curPlayers, int _maxPlayers)
+    {
+        id = _id;
+        roomName = _roomName;
+        ownerName = _ownerName;
+        curPlayers = _curPlayers;
+        maxPlayers = _maxPlayers;
+    }
+}
+public class RoomInfo : MonoBehaviour
+{
+    public int roomId;
+
+    public Text roomIdText;
+    public Text roomNameText;
+    public Text roomPlayersText;
+
+}
