e00fbc96abab038cc97bd2588abc71476627470e
MinShiGee <alstjr1642@ajou.ac.kr>
2021-01-22 06:03:31 +0900

    Adding Exit Lobby, Room, Game

--- Assets/Scripts/Lobby/LobbyUIManager.cs
+++ Assets/Scripts/Lobby/LobbyUIManager.cs
@@ -70,18 +70,25 @@ public class LobbyUIManager : MonoBehaviour
         mainLobbyUI.SetActive(true);
         gameLobbyUI.SetActive(false);
         roomLobbyUI.SetActive(false);
+        Client.instance.Disconnect();
     }
     public void ShowGameLobby()
     {
         mainLobbyUI.SetActive(false);
         gameLobbyUI.SetActive(true);
         roomLobbyUI.SetActive(false);
+        ClientSend.JoinRoom(-1);
     }
     public void ShowRoomLobby()
     {
         mainLobbyUI.SetActive(false);
         gameLobbyUI.SetActive(false);
         roomLobbyUI.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 
     #endregion
